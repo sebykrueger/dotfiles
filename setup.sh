17a24fd31883f8c66078712b39459509d1d1f800
sebykrueger <email@sebkrueger.info>
2024-05-04 11:14:41 +1200

    updates

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ function doIt() {
 	rsync --exclude ".git/" \
 		--exclude ".DS_Store" \
 		--exclude ".osx" \
-		--exclude "bootstrap.sh" \
+		--exclude "setup.sh" \
 		--exclude "README.md" \
 		--exclude "LICENSE-MIT.txt" \
 		-avh --no-perms . ~;
